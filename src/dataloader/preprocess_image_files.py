14e3ed7c54844da070d6cddb63bdbcac6c65ddc8
rrahman3 <rrahman3@uncc.edu>
2024-10-23 09:48:05 -0400

    copy image file class struture

--- src/dataloader/preprocess_image_files.py
+++ src/dataloader/preprocess_image_files.py
@@ -2,40 +2,65 @@ import shutil
 import os
 import pandas as pd
 
-train_df = pd.read_csv('../data/nihcc_chest_xray/nihcc_chest_xray_training_samples.csv')
+class CopyImages:
+    def __init__(self, metadata_file, src_dir, dst_dir):
+        self.metadata_file = metadata_file
+        self.train_df = pd.read_csv(metadata_file)
 
-classes = ['Atelectasis', 'Cardiomegaly', 'Consolidation',
-       'Edema', 'Effusion', 'Emphysema', 'Fibrosis', 'Hernia', 'Infiltration',
-       'Mass', 'Nodule', 'Pleural_Thickening', 'Pneumonia', 'Pneumothorax',
-       'Pneumoperitoneum', 'Pneumomediastinum', 'Subcutaneous_Emphysema',
-       'Tortuous_Aorta', 'Calcification_of_the_Aorta', 'No_Finding']
+        self.classes = ['Atelectasis', 'Cardiomegaly', 'Consolidation',
+            'Edema', 'Effusion', 'Emphysema', 'Fibrosis', 'Hernia', 'Infiltration',
+            'Mass', 'Nodule', 'Pleural_Thickening', 'Pneumonia', 'Pneumothorax',
+            'Pneumoperitoneum', 'Pneumomediastinum', 'Subcutaneous_Emphysema',
+            'Tortuous_Aorta', 'Calcification_of_the_Aorta', 'No_Finding']
 
-train_df = train_df.rename(columns={'Subcutaneous Emphysema':'Subcutaneous_Emphysema', 'Pleural Thickening': 'Pleural_Thickening', 'SubcutaneousEmphysema': 'Subcutaneous_Emphysema', 'Tortuous Aorta':'Tortuous_Aorta', 'Calcification of the Aorta':'Calcification_of_the_Aorta', 'No Finding':'No_Finding'})
+        self.train_df = self.train_df.rename(columns={'Subcutaneous Emphysema':'Subcutaneous_Emphysema', 
+                                                      'Pleural Thickening': 'Pleural_Thickening', 
+                                                      'SubcutaneousEmphysema': 'Subcutaneous_Emphysema', 
+                                                      'Tortuous Aorta':'Tortuous_Aorta', 
+                                                      'Calcification of the Aorta':'Calcification_of_the_Aorta', 
+                                                      'No Finding':'No_Finding'})
 
-src_dir = "data/nihcc_chest_xray/xray_images"
-dst_dir = "data/nihcc_chest_xray/images/train"
-os.makedirs("data/nihcc_chest_xray/images/")
-os.makedirs("data/nihcc_chest_xray/images/train/")
-for class_name in classes:
-    class_dir = os.path.join(dst_dir, class_name)
-    if not os.path.exists(class_dir):
-        os.makedirs(class_dir)
-        
-def find_classes(row):
-    class_lists = []
-    for class_name in classes:
-        if row[class_name]:
-            class_lists.append(class_name)
-        
-    return class_lists
+        self.src_dir = src_dir
+        self.dst_dir = dst_dir
+        os.makedirs(self.dst_dir, exist_ok=True)
+        for class_name in self.classes:
+            class_dir = os.path.join(dst_dir, class_name)
+            if not os.path.exists(class_dir):
+                os.makedirs(class_dir, exist_ok=True)
+                
+    def find_classes(self, row):
+        class_lists = []
+        for class_name in self.classes:
+            if row[class_name]:
+                class_lists.append(class_name)
+            
+        return class_lists
 
-for idx, row in train_df.iterrows():
-    # print(row)
-    img_file = row['id']
-    class_lables = find_classes(row)
-    source_img_path = os.path.join(src_dir, img_file)
-    if os.path.exists(source_img_path):
-        for class_name in class_lables:
-            target_img_path = os.path.join(dst_dir, class_name, img_file)
-            shutil.copy(source_img_path, target_img_path)
-        print(idx, source_img_path, target_img_path)
+    def copy_files(self):
+        for idx, row in self.train_df.iterrows():
+            # print(row)
+            img_file = row['id']
+            class_lables = self.find_classes(row)
+            source_img_path = os.path.join(self.src_dir, img_file)
+
+            if os.path.exists(source_img_path):
+                for class_name in class_lables:
+                    target_img_path = os.path.join(self.dst_dir, class_name, img_file)
+                    if os.path.exists(source_img_path):
+                        print("Already destination file exists.")
+                        continue
+                    shutil.copy(source_img_path, target_img_path)
+                print(idx, source_img_path, target_img_path)
+
+if __name__=="__main__":
+    # train = CopyImages(metadata_file='data/nihcc_chest_xray/nihcc_chest_xray_training_samples.csv',
+    #             src_dir="data/nihcc_chest_xray/xray_images",
+    #             dst_dir="data/nihcc_chest_xray/images/train",
+    #         )
+    # train.copy_files()
+
+    val = CopyImages(metadata_file='data/nihcc_chest_xray/nihcc_chest_xray_validation_samples.csv',
+                src_dir="data/nihcc_chest_xray/xray_images",
+                dst_dir="data/nihcc_chest_xray/images/val",
+            )
+    val.copy_files()
