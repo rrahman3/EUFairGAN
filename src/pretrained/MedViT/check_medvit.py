8c270ad2fb0430492b7d112b6ced61af780a092c
rrahman3 <rrahman3@uncc.edu>
2024-10-24 12:19:31 -0400

    testing medvit training

--- src/pretrained/MedViT/check_medvit.py
+++ src/pretrained/MedViT/check_medvit.py
@@ -36,94 +36,104 @@ import torch.optim as optim
 import torch.utils.data as data
 import torchvision.transforms as transforms
 
-import medmnist
-from medmnist import INFO, Evaluator
+# import medmnist
+# from medmnist import INFO, Evaluator
 
-data_flag = 'breastmnist'
-data_flag = 'chestmnist'
-# [tissuemnist, pathmnist, chestmnist, dermamnist, octmnist, 
-# pnemoniamnist, retinamnist, breastmnist, bloodmnist, tissuemnist, organamnist, organcmnist, organsmnist]
-download = True
+# data_flag = 'breastmnist'
+# data_flag = 'chestmnist'
+# # [tissuemnist, pathmnist, chestmnist, dermamnist, octmnist, 
+# # pnemoniamnist, retinamnist, breastmnist, bloodmnist, tissuemnist, organamnist, organcmnist, organsmnist]
+# download = True
 
 NUM_EPOCHS = 100
 BATCH_SIZE = 32
 lr = 0.005
 
-info = INFO[data_flag]
-task = info['task']
-n_channels = info['n_channels']
-n_classes = len(info['label'])
+# info = INFO[data_flag]
+# task = info['task']
+# n_channels = info['n_channels']
+# n_classes = len(info['label'])
 
-DataClass = getattr(medmnist, info['python_class'])
+# DataClass = getattr(medmnist, info['python_class'])
 
 
-from torchvision.transforms.transforms import Resize
-# preprocessing
-train_transform = transforms.Compose([
-    transforms.Resize(224),
-    transforms.Lambda(lambda image: image.convert('RGB')),
-    torchvision.transforms.AugMix(),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[.5], std=[.5])
-])
-test_transform = transforms.Compose([
-    transforms.Resize(224),
-    transforms.Lambda(lambda image: image.convert('RGB')),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[.5], std=[.5])
-])
+# from torchvision.transforms.transforms import Resize
+# # preprocessing
+# train_transform = transforms.Compose([
+#     transforms.Resize(224),
+#     transforms.Lambda(lambda image: image.convert('RGB')),
+#     torchvision.transforms.AugMix(),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[.5], std=[.5])
+# ])
+# test_transform = transforms.Compose([
+#     transforms.Resize(224),
+#     transforms.Lambda(lambda image: image.convert('RGB')),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[.5], std=[.5])
+# ])
 
-# load the data
-train_dataset = DataClass(split='train', transform=train_transform, download=download)
-test_dataset = DataClass(split='test', transform=test_transform, download=download)
+# # load the data
+# train_dataset = DataClass(split='train', transform=train_transform, download=download)
+# test_dataset = DataClass(split='test', transform=test_transform, download=download)
 
-# pil_dataset = DataClass(split='train', download=download)
+# # pil_dataset = DataClass(split='train', download=download)
 
-# encapsulate data into dataloader form
-train_loader = data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-train_loader_at_eval = data.DataLoader(dataset=train_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
-test_loader = data.DataLoader(dataset=test_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
+# # encapsulate data into dataloader form
+# train_loader = data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+# train_loader_at_eval = data.DataLoader(dataset=train_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
+# test_loader = data.DataLoader(dataset=test_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
 
 # define loss function and optimizer
-if task == "multi-label, binary-class":
-    criterion = nn.BCEWithLogitsLoss()
-else:
-    criterion = nn.CrossEntropyLoss()
+# if task == "multi-label, binary-class":
+criterion = nn.BCEWithLogitsLoss()
+# else:
+#     criterion = nn.CrossEntropyLoss()
     
 optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
 
+from src.dataloader.medical_dataset import NIHChestXrayDataset
+
+dataset = NIHChestXrayDataset(
+        metadata_file="../data/nihcc_chest_xray/nihcc_chest_xray_training_samples.csv",
+        image_dir="../data/nihcc_chest_xray/xray_images/",
+        frac=0.01,
+        isTest=False,
+    )
+
+train_loader = data.DataLoader(
+        dataset,
+        batch_size=32,
+        shuffle=True,
+    )
+
 
 # evaluation
-def test(split):
-    model.eval()
-    y_true = torch.tensor([]).to(device)
-    y_score = torch.tensor([]).to(device)
+# def test(split):
+#     model.eval()
+#     y_true = torch.tensor([]).to(device)
+#     y_score = torch.tensor([]).to(device)
     
-    data_loader = train_loader_at_eval if split == 'train' else test_loader
+#     data_loader = train_loader_at_eval if split == 'train' else test_loader
 
-    with torch.no_grad():
-        for inputs, targets in data_loader:
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = model(inputs)
+#     with torch.no_grad():
+#         for inputs, targets in data_loader:
+#             inputs, targets = inputs.to(device), targets.to(device)
+#             outputs = model(inputs)
 
-            if task == 'multi-label, binary-class':
-                targets = targets.to(torch.float32)
-                outputs = outputs.softmax(dim=-1)
-            else:
-                targets = targets.squeeze().long()
-                outputs = outputs.softmax(dim=-1)
-                targets = targets.float().resize_(len(targets), 1)
+#             targets = targets.to(torch.float32)
+#             outputs = outputs.softmax(dim=-1)
 
-            y_true = torch.cat((y_true, targets), 0)
-            y_score = torch.cat((y_score, outputs), 0)
+#             y_true = torch.cat((y_true, targets), 0)
+#             y_score = torch.cat((y_score, outputs), 0)
 
-        y_true = y_true.cpu().numpy()
-        y_score = y_score.detach().cpu().numpy()
+#         y_true = y_true.cpu().numpy()
+#         y_score = y_score.detach().cpu().numpy()
         
-        evaluator = Evaluator(data_flag, split)
-        metrics = evaluator.evaluate(y_score)
+#         # evaluator = Evaluator(data_flag, split)
+#         # metrics = evaluator.evaluate(y_score)
     
-        print('%s  auc: %.3f  acc:%.3f' % (split, *metrics))
+#         # print('%s  auc: %.3f  acc:%.3f' % (split, *metrics))
 
 # train
 def train():
@@ -143,12 +153,8 @@ def train():
             optimizer.zero_grad()
             outputs = model(inputs)
             
-            if task == 'multi-label, binary-class':
-                targets = targets.to(torch.float32)
-                loss = criterion(outputs, targets)
-            else:
-                targets = targets.squeeze().long()
-                loss = criterion(outputs, targets)
+            targets = targets.to(torch.float32)
+            loss = criterion(outputs, targets)
             
             loss_log.append(loss.item())
             epoch_log.append(epoch)
@@ -157,7 +163,7 @@ def train():
             optimizer.step()
             num_batch += 1
         
-        test('test')
+        # test('test')
         import pandas as pd
         df = pd.DataFrame({
             'epoch': epoch_log,
@@ -172,7 +178,7 @@ def train():
 train()
 
         
-print('==> Evaluating ...')
-test('train')
-test('test')
+# print('==> Evaluating ...')
+# test('train')
+# test('test')
 
