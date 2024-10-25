558b5cf3bc3978025bcc6b17f7aba15a44b785f4
rrahman3 <rrahman3@uncc.edu>
2024-10-25 11:01:42 -0400

    bnn medvit training testing.

--- src/pretrained/MedViT/BNN_MedViT_Training.py
+++ src/pretrained/MedViT/BNN_MedViT_Training.py
@@ -108,7 +108,7 @@ validation_loader = data.DataLoader(validation_dataset, batch_size=32, shuffle=T
 
 test_dataset = NIHChestXrayDataset(metadata_file="data/nihcc_chest_xray/nihcc_chest_xray_testing_samples.csv",
         image_dir="data/nihcc_chest_xray/xray_images/", 
-        frac=1.0, isTest=False)
+        frac=1.0, isTest=True)
 
 test_loader = data.DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
 male_test_loader = data.DataLoader(test_dataset.filter_by_gender('male'), batch_size=32, shuffle=False, num_workers=4)
